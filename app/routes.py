efb2eb42058d1cf1749786bc57892fb3ca00fec3
Niko-Hems <niko.hems@gmail.com>
2024-07-02 12:42:54 +0200

    add germany to charts

--- app/routes.py
+++ app/routes.py
@@ -4,11 +4,9 @@ from app.services.worldbank_api import get_gdp_data, get_population_data
 main = Blueprint('main', __name__)
 
 @main.route('/')
-def index():
-    return render_template('index.html')
-
-@main.route('/dashboard')
 def dashboard():
-    gdp_years, gdp_values = get_gdp_data()
-    population_years, population_values = get_population_data()
-    return render_template('dashboard.html', gdp_years=gdp_years, gdp_values=gdp_values, population_years=population_years, population_values=population_values)
+    gdp_years, us_gdp_values, de_gdp_values = get_gdp_data()
+    population_years, us_population_values, de_population_values = get_population_data()
+    return render_template('dashboard.html', 
+                           gdp_years=gdp_years, us_gdp_values=us_gdp_values, de_gdp_values=de_gdp_values, 
+                           population_years=population_years, us_population_values=us_population_values, de_population_values=de_population_values)
--- app/services/worldbank_api.py
+++ app/services/worldbank_api.py
@@ -5,22 +5,26 @@ def fetch_worldbank_data(indicator, country, start_year, end_year):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        return data[1]  # The actual data is in the second element of the returned list
-    else:
-        return None
+        if data and len(data) > 1:
+            return data[1]  # The actual data is in the second element of the returned list
+    return None
 
 def get_gdp_data():
-    data = fetch_worldbank_data('NY.GDP.MKTP.CD', 'US', 2011, 2021)
-    if data:
-        years = [item['date'] for item in data]
-        values = [item['value'] for item in data]
-        return years, values
-    return [], []
+    us_data = fetch_worldbank_data('NY.GDP.MKTP.CD', 'US', 2011, 2021)
+    de_data = fetch_worldbank_data('NY.GDP.MKTP.CD', 'DE', 2011, 2021)
+    if us_data and de_data:
+        years = [item['date'] for item in us_data if item['value'] is not None]
+        us_values = [item['value'] for item in us_data if item['value'] is not None]
+        de_values = [item['value'] for item in de_data if item['value'] is not None]
+        return years, us_values, de_values
+    return [], [], []
 
 def get_population_data():
-    data = fetch_worldbank_data('SP.POP.TOTL', 'US', 2011, 2021)
-    if data:
-        years = [item['date'] for item in data]
-        values = [item['value'] for item in data]
-        return years, values
-    return [], []
+    us_data = fetch_worldbank_data('SP.POP.TOTL', 'US', 2011, 2021)
+    de_data = fetch_worldbank_data('SP.POP.TOTL', 'DE', 2011, 2021)
+    if us_data and de_data:
+        years = [item['date'] for item in us_data if item['value'] is not None]
+        us_values = [item['value'] for item in us_data if item['value'] is not None]
+        de_values = [item['value'] for item in de_data if item['value'] is not None]
+        return years, us_values, de_values
+    return [], [], []
